89f269080de3f30a62e57b922ef0ee656039f1ef
Ethan Fischer <45436178+ejfisc@users.noreply.github.com>
2022-02-08 14:20:43 -0600

    Chapter 7

--- Chapter7/regex_objects.py
+++ Chapter7/regex_objects.py
@@ -1,3 +1,4 @@
+from decimal import ConversionSyntax
 import re
 # passing a string to re.compile() returns a regex pattern object
 # \d means 'a digit character' and \d\d\d-\d\d\d-\d\d\d\d is the regular expression (regex)
@@ -100,3 +101,114 @@ phoneNumRegex = re.compile(r'(\d\d\d)-(\d\d\d)-(\d\d\d\d)')
 mo = phoneNumRegex.findall('Cell: 415-555-9999 Work: 212-555-0000')
 print(mo)
 
+# shorthand character classes:
+# \d - any numeric digit from 0 to 9
+# \D - any character that is not a numeric digit from 0 to 9
+# \w - Any letter, numeric digit, or the underscore character
+# \W - any character that is not a letter, numeric digit, or the underscore character
+# \s - any space, tab, or newline character
+# \S - any character that is not a space, tab or newline
+
+# these are useful for shortening regex. The character class [0-5] will 
+# match numbers from 0 to 5, and is much shorter than typing (0|1|2|3|4|5)
+# there is no shorthand class that matches only letters, but you can use [a-zA-Z] class
+
+xmasRegex = re.compile(r'\d+\s\w+')
+print(xmasRegex.findall('12 drummers, 11 pipers, 10 lords, 9 ladies, 8 maids, 7 swans, 6 geese, 5 rings, 4 birds, 3 hens, 2 doves, 1 partridge'))
+
+# you can define your own character class using the square brackets
+vowelRegex = re.compile(r'[aeiouAEIOU]')
+print(vowelRegex.findall('RoboCop eats baby food. BABY FOOD.'))
+letNumRegex = re.compile(r'[a-zA-Z0-9]') # will match all lowercase and uppercase letters, and numbers
+# note that inside square brackets you dont need to escape meaningul symbols
+# the character class [0-5.] will match digits 0-5 and a period, no need to write it as [0-5\.]
+
+# by placing a caret character just after the class opening bracket, you can make a negative character class
+# a negative character class will match all the characters that are not in the class
+consonantRegex = re.compile(r'[^aeiouAEIOU]')
+print(consonantRegex.findall('RoboCop eats baby food. BABY FOOD.'))
+
+# the ^ can also be used at the start of a regex to indicate that a match must occur
+# at the beginning of the searched text
+beginsWithHello = re.compile(r'^Hello')
+print(beginsWithHello.search('Hello, world!'))
+print(beginsWithHello.search('He said Hello.'))
+
+# the $ can be used at the end of a regex to indicate that a match must occur
+# at the end of the searched text
+endsWithNumber = re.compile(r'\d$')
+print(endsWithNumber.search('Your number is 42'))
+print(endsWithNumber.search('42 is your number'))
+
+# you can use multiple special characters to be even more specific
+wholeStringIsNum = re.compile(r'^\d+$')
+print(wholeStringIsNum.search('1234567890')) # match
+print(wholeStringIsNum.search('123abc456')) # none
+print(wholeStringIsNum.search('123 456')) # none
+
+# the . character in a regex is called a wildcard and will match any character except newline
+atRegex = re.compile(r'.at')
+print(atRegex.findall('The cat in the hat sat on the flat mat.'))
+# the . character only matches one character. To match an actual dot, escape the . with a backslash
+
+# you can use .* to match anything and everything
+nameRegex = re.compile(r'First Name: (.*) Last Name: (.*)')
+mo = nameRegex.search('First Name: Ethan Last Name: Fischer')
+print(mo.group(1)) # 'Ethan'
+print(mo.group(2)) # 'Fischer'
+# the .* uses greedy matching, to match any text in a non-greedy fashion, use .*?
+nonGreedyRegex = re.compile(r'<.*?>')
+mo = nonGreedyRegex.search('<To serve man> for dinner.>')
+print(mo.group()) # '<To serve man>'
+greedyRegex = re.compile(r'<.*>')
+mo = greedyRegex.search('<To serve man> for dinner.>')
+print(mo.group())
+
+# technically, .* will match everything except a newline, but if you want to include newline,
+# pass re.DOTALL as the second argument to re.compile()
+noNewLineRegex = re.compile(r'.*')
+mo = noNewLineRegex.search('Serve the public trust. \nProtect the innocent. \nUphold the law.')
+print(mo.group())
+newLineRegex = re.compile(r'.*', re.DOTALL)
+mo = newLineRegex.search('Serve the public trust. \nProtect the innocent. \nUphold the law.')
+print(mo.group())
+
+# by default, regex are case sensitive
+# you can pass re.IGNORECASE or re.I as a second argument to re.compile() to match the letters regardless of case
+robocop = re.compile(r'robocop', re.I)
+print(robocop.search('robocop').group())
+print(robocop.search('RoBoCoP').group())
+print(robocop.search('ROBOCOP').group())
+
+# the sub() method can substitute new text in place of matched regex patterns
+# sub() is passed 2 arguments, a string to replace any matches, and a string for the regex
+# the sub() method returns a string with the substitutions applied
+namesRegex = re.compile(r'Agent \w+')
+censored = namesRegex.sub('CENSORED', 'Agent Alice gave the secret documents to Agent Bob.')
+print(censored)
+# you may need to use the matched text itself as part of the substitution. In the first argument to sub, you can
+# type \1, \2, \3 and so on to mean 'Enter the text of group 1, 2, 3 and so on in the substitution'
+# i.e. let's say you want to censor the names of the secret agents by showing just the first letters of their names
+# use the regex Agent (\w)\w* and pass r'\1****' as the first argument to sub()
+agentNamesRegex = re.compile(r'Agent (\w)\w*')
+censored = agentNamesRegex.sub(r'\1****', 'Agent Alice told Agent Carol that Agent Eve knew Agent Bob was a double agent.')
+print(censored)
+
+# matching complicated text patterns might require long, convoluted regex. You can mitigate this by telling
+# re.compile() to ignore whitespace and comments inside the expression string (verbose mode)
+# by passing re.VERBOSE as the second argument to re.compile()
+phoneRegex = re.compile(r'''(
+    (\d{3}|\(\d{3}\))?              # area code
+    (\s|-|\.)?                      # separator
+    \d{3}                           # first 3 digits
+    (\s|-|\.)                       # separator
+    \d{4}                           # last 4 digits
+    (\s*(ext|x|ext.)\s*\d{2,5})?    # extension
+    )''', re.VERBOSE)
+
+# if you want to combine re.IGNORECASE, re.DOTALL, and re.VERBOSE, re.compile() only takes 2 arguments
+# but you can use | (in this context it is the bitwise or operator)
+# so if you want a regex that's case sensitive and includes newlines to match the dot character:
+someRegexValue = re.compile('foo', re.IGNORECASE | re.DOTALL)
+# including all 3:
+someRegexValue = re.compile('foo', re.IGNORECASE | re.DOTALL | re.VERBOSE)
